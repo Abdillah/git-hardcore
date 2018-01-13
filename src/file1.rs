11d62760a52608f6f7c8a7df1454823a4187ea37
Hernawan Fa'iz Abdillah <hernawan.faiz@abdillah.me>
2018-01-13 13:55:59 +0700

    src/file1: Refactor second par

--- src/file1.rs
+++ src/file1.rs
@@ -16,6 +16,13 @@
 */
 
 /**
-* Cras vel enim ullamcorper, venenatis enim quis, tristique sem. Maecenas molestie augue in dolor tincidunt, ut facilisis diam posuere. Pellentesque eu mattis velit. Suspendisse et venenatis metus, a sodales ligula. Mauris dapibus dolor arcu, fringilla finibus leo convallis eu. Proin mattis, magna id dignissim luctus, enim lorem placerat lacus, eu volutpat lacus nibh nec orci. In id sapien rutrum, commodo erat sit amet, tincidunt nibh. Sed placerat, nulla at placerat ornare, ex ante
-* rutrum eros, vel faucibus lectus neque eget lorem. Nulla id magna nibh. Curabitur cursus euismod mi, sit amet cursus purus consectetur a. Praesent et cursus nunc, at sagittis quam. Integer porttitor vehicula fermentum.
+* Cras vel enim ullamcorper, venenatis enim quis, tristique sem. Maecenas molestie augue
+* in dolor tincidunt, ut facilisis diam posuere. Pellentesque eu mattis velit.
+* Suspendisse et venenatis metus, a sodales ligula. Mauris dapibus dolor arcu,
+* fringilla finibus leo convallis eu. Proin mattis, magna id dignissim luctus,
+* enim lorem placerat lacus, eu volutpat lacus nibh nec orci. In id sapien rutrum, commodo erat sit amet,
+* tincidunt nibh. Sed placerat, nulla at placerat ornare, ex ante rutrum eros,
+* vel faucibus lectus neque eget lorem. Nulla id magna nibh. Curabitur cursus euismod mi,
+* sit amet cursus purus consectetur a. Praesent et cursus nunc, at sagittis quam.
+* Integer porttitor vehicula fermentum.
 */
